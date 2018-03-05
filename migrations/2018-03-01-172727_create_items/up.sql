b0236dc1c37ebe33d910c260cf2d084713e84a66
Colin Benner <colin@yzhs.de>
2018-03-05 20:42:11 +0100

    backend: Update 'items' table to use TEXT for date/time fields

--- migrations/2018-03-01-172727_create_items/up.sql
+++ migrations/2018-03-01-172727_create_items/up.sql
@@ -4,13 +4,13 @@ CREATE TABLE items (
 	employee_id integer NOT NULL REFERENCES employees(id),
 
 
-	start_datetime integer NOT NULL CHECK(
+	start_datetime text NOT NULL CHECK(
 		date(start_datetime) >= '2017-08-01' AND
 		time(start_datetime) >= '11:00'
 	),
-	end_datetime integer NOT NULL CHECK (
+	end_datetime text NOT NULL CHECK (
 		date(start_datetime) = date(end_datetime) AND
-		start_datetime <= end_datetime - 15*60 AND
+		time(start_datetime) <= time(end_datetime, '- 15minutes') AND
 		time(end_datetime) <= '16:00'
 	),
 
