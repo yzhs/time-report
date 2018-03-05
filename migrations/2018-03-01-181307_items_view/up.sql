8f2fb5a2b5d56c0df5189c63d66486798bba8943
Colin Benner <colin@yzhs.de>
2018-03-05 20:42:34 +0100

    backend: Reformat CREATE VIEW items_view

--- migrations/2018-03-01-181307_items_view/up.sql
+++ migrations/2018-03-01-181307_items_view/up.sql
@@ -1,10 +1,14 @@
 CREATE VIEW items_view AS
-SELECT items.id AS id, employees.name AS name,
-	date(start_datetime) AS day, type_of_week,
-	time(start_datetime) AS start, time(end_datetime) AS stop,
+SELECT items.id AS id,
+	employees.name AS name,
+	date(start_datetime) AS day,
+	type_of_week,
+	time(start_datetime) AS start,
+	time(end_datetime) AS end,
 	remark
 FROM items
-JOIN employees ON items.employee_id = employees.id
-JOIN weeks ON
+JOIN employees
+JOIN weeks
+ON items.employee_id = employees.id AND
 	cast(strftime('%y', start_datetime) AS integer) = weeks.year AND
 	cast(strftime('%W', start_datetime) AS integer) = weeks.week_of_year
