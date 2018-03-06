490af67a02a32efbb7be96c4657cdae59f9a5324
Colin Benner <colin@yzhs.de>
2018-03-06 16:38:14 +0100

    backend: Add function get_employees()

--- src/lib.rs
+++ src/lib.rs
@@ -154,9 +154,31 @@ pub fn create_item(conn: &SqliteConnection, new_row: NewRow) {
     diesel::insert_into(items::table).values(&new_item);
 }
 
+pub fn get_employees(conn: &SqliteConnection) -> Vec<String> {
+    use schema::employees;
+    employees::table
+        .select(employees::name)
+        .load::<String>(conn)
+        .unwrap()
+}
+
 #[cfg(test)]
 mod test {
     use super::*;
+
+    fn empty_tables(conn: &SqliteConnection) {
+        diesel::delete(schema::employees::table)
+            .execute(conn)
+            .unwrap();
+        diesel::delete(schema::items::table).execute(conn).unwrap();
+        diesel::delete(schema::holidays::table)
+            .execute(conn)
+            .unwrap();
+        diesel::delete(schema::reports::table)
+            .execute(conn)
+            .unwrap();
+        diesel::delete(schema::weeks::table).execute(conn).unwrap();
+    }
 
     #[test]
     fn test_create_employee() {
@@ -168,5 +190,18 @@ mod test {
         assert_ne!(id, id2);
         assert_eq!(create_employee(&conn, "Alice A.").unwrap(), id);
         assert_eq!(create_employee(&conn, "Bob B.").unwrap(), id2);
+    }
+
+    #[test]
+    fn test_get_employees() {
+        let conn = establish_connection();
+        empty_tables(&conn);
+
+        let names = vec!["Alice A.", "Bob B.", "Charlie C."];
+        for name in &names {
+            create_employee(&conn, name).unwrap();
+        }
+
+        assert_eq!(get_employees(&conn), names);
     }
 }
