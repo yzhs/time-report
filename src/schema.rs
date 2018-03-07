d678712c0568ebed592b02e2c14fd4e5188421b1
Colin Benner <colin@yzhs.de>
2018-03-07 12:11:59 +0100

    backend: Update column names for items_view

--- src/schema.rs
+++ src/schema.rs
@@ -1,13 +1,13 @@
 infer_schema!("dotenv:DATABASE_URL");
 
 table! {
-    items_view (id, name, day, type_of_week, start, stop, remark) {
+    items_view (id, name, day, type_of_week, start, end, remark) {
         id -> Integer,
         name -> Text,
         day -> Date,
         type_of_week -> Integer,
         start -> Time,
-        stop -> Time,
+        end -> Time,
         remark -> Text,
     }
 }
