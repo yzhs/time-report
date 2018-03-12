70bcc2f46031df58d422c80ea1a2f6c30791fa95
Colin Benner <colin@yzhs.de>
2018-03-12 17:19:02 +0100

    backend: Merge lib.rs into main.rs
    Do not separate the web-part from the database part. The separation only
    lead to awkward code and a bunch of importing.

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,66 @@
 #![feature(plugin)]
 #![plugin(rocket_codegen)]
+#![recursion_limit = "128"]
+
+extern crate chrono;
+
+extern crate curl;
+
+#[macro_use]
+pub extern crate diesel;
+#[macro_use]
+extern crate diesel_infer_schema;
+
+extern crate dotenv;
+
+#[macro_use]
+extern crate lazy_static;
+
+#[macro_use]
+extern crate log;
 
 extern crate rocket;
 extern crate rocket_contrib;
 extern crate rocket_cors;
 
-extern crate time_report;
+extern crate serde;
+#[macro_use]
+extern crate serde_derive;
+extern crate serde_json;
 
 use std::path::{Path, PathBuf};
 
 use rocket::response::NamedFile;
 use rocket_contrib::Json;
 
-use time_report::globals::Globals;
-use time_report::items::{InvoiceItem, NewRow};
-use time_report::reports::{self, Report};
+use items::{InvoiceItem, NewRow};
+use reports::Report;
+
+pub mod db;
+pub mod employees;
+pub mod holidays;
+pub mod items;
+pub mod reports;
+pub mod schema;
+pub mod weeks;
+
+pub const DATE_FORMAT: &str = "%Y-%m-%d";
+pub const TIME_FORMAT: &str = "%H:%M";
+
+#[derive(Serialize)]
+pub struct Globals {
+    pub mintime: &'static str,
+    pub maxtime: &'static str,
+}
+
+impl Globals {
+    pub fn new() -> Globals {
+        Globals {
+            mintime: "12:30",
+            maxtime: "16:00",
+        }
+    }
+}
 
 #[get("/")]
 fn index() -> Option<NamedFile> {
@@ -34,49 +80,49 @@ fn files(file: PathBuf) -> Option<NamedFile> {
 // TODO better name
 #[get("/globals", format = "application/json")]
 fn get_globals() -> Json<Globals> {
-    Json(time_report::globals::get())
+    Json(Globals::new())
 }
 
 // TODO better name
 #[get("/items", format = "application/json")]
 fn get_items() -> Json<Vec<InvoiceItem>> {
-    let conn = time_report::db::connect();
-    Json(time_report::items::get(&conn))
+    let conn = db::connect();
+    Json(items::get(&conn))
 }
 
 #[get("/items/template", format = "application/json")]
 fn item_template() -> Json<InvoiceItem> {
-    let conn = time_report::db::connect();
-    Json(time_report::items::template(&conn))
+    let conn = db::connect();
+    Json(items::template(&conn))
 }
 
 #[put("/items/<id>", format = "application/json", data = "<item>")]
 fn set_item(id: i32, item: Json<NewRow>) -> Json<i32> {
-    let conn = time_report::db::connect();
-    Json(time_report::items::update(&conn, id, item.into_inner()))
+    let conn = db::connect();
+    Json(items::update(&conn, id, item.into_inner()))
 }
 
 #[get("/employees", format = "application/json")]
 fn get_employees() -> Json<Vec<String>> {
-    let conn = time_report::db::connect();
-    Json(time_report::employees::get(&conn))
+    let conn = db::connect();
+    Json(employees::get(&conn))
 }
 
 #[get("/holidays", format = "application/json")]
 fn get_holidays() -> Json<std::collections::HashMap<String, String>> {
-    let conn = time_report::db::connect();
-    Json(time_report::holidays::get(&conn))
+    let conn = db::connect();
+    Json(holidays::get(&conn))
 }
 
 #[get("/reports", format = "application/json")]
 fn get_reports() -> Json<Vec<Report>> {
-    let conn = time_report::db::connect();
+    let conn = db::connect();
     Json(reports::get(&conn))
 }
 
 #[post("/reports", format = "application/json", data = "<report>")]
 fn add_report(report: Json<Report>) {
-    let conn = time_report::db::connect();
+    let conn = db::connect();
     reports::add(&conn, report.into_inner());
 }
 
