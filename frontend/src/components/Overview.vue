5782a1bcd4fe97912473114996f0bc2cb5e9c3c2
Colin Benner <colin@yzhs.de>
2018-03-13 16:35:12 +0100

    frontend: Much nicer overview

--- frontend/src/components/Overview.vue
+++ frontend/src/components/Overview.vue
@@ -2,11 +2,25 @@
   <div id="app">
       <h1>Abrechnungen</h1>
 
-      <ul>
-        <li v-for="report in reports" :key="report.id">
-          {{report.title}}
-        </li>
-      </ul>
+      <table>
+        <thead>
+          <th>Titel</th>
+          <th>von</th>
+          <th>bis</th>
+        </thead>
+        <tbody>
+          <tr class="report" v-for="report in reports" :key="report.id">
+            <td><input type="text" name="title" v-model="report.title"></td>
+            <td><input type="date" name="start_date" v-model="report.start_date"></td>
+            <td><input type="date" name="end_date" v-model="report.end_date"></td>
+            <td>
+              <router-link :to="{name: 'report', params: {id: report.id}}">Bearbeiten</router-link>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+
+      <button name="new-report" v-on:click="newReport">Neue Abrechnung</button>
   </div>
 </template>
 
@@ -20,19 +34,43 @@ export default {
   beforeMount () {
     this.$http.get('reports').then(response => {
       response.body.forEach(element => {
+        element.in_db = true
         this.reports.push(element)
       })
     })
+  },
+  methods: {
+    newReport () {
+      let lastReport = this.reports[this.reports.length - 1]
+      let reportTemplate = {
+        id: lastReport.id + 1,
+        title: '',
+        start_date: lastReport.end_date,
+        end_date: '2018-01-01',
+        was_pdf_generated: false,
+        in_db: false
+      }
+      this.reports.push(reportTemplate)
+    }
   }
 }
 </script>
 
 <style>
-ul {
-  list-style-type: none;
+body {
+  text-align: center;
 }
 
-li {
-  font-size: 120%;
+input {
+  background: none;
+  border: 0;
+}
+
+button {
+  border-radius: 0;
+  color: white;
+  background: #369;
+  padding: 0.5em;
+  margin: 1em;
 }
 </style>
