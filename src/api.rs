6d1d501917cad7826906206709d4787c3c824703
Colin Benner <colin@yzhs.de>
2018-03-13 12:29:18 +0100

    backend: Declutter main.rs
    Move API endpoints to api.rs.

---
+++ src/api.rs
@@ -0,0 +1,83 @@
+use std::collections::HashMap;
+
+use rocket_contrib::Json;
+
+use db;
+use employees;
+use items::{self, InvoiceItem, NewRow};
+use holidays;
+use reports::{self, Report};
+
+#[derive(Serialize)]
+pub struct Globals {
+    pub mintime: &'static str,
+    pub maxtime: &'static str,
+}
+
+impl Globals {
+    pub fn new() -> Globals {
+        Globals {
+            mintime: "12:30",
+            maxtime: "16:00",
+        }
+    }
+}
+
+#[get("/globals", format = "application/json")]
+fn get_globals() -> Json<Globals> {
+    Json(Globals::new())
+}
+
+#[get("/items", format = "application/json")]
+fn get_items(conn: db::DbConn) -> Json<Vec<InvoiceItem>> {
+    Json(items::get(&conn))
+}
+
+#[get("/items/template", format = "application/json")]
+fn item_template(conn: db::DbConn) -> Json<InvoiceItem> {
+    Json(items::template(&conn))
+}
+
+#[put("/items/<id>", format = "application/json", data = "<item>")]
+fn set_item(conn: db::DbConn, id: i32, item: Json<NewRow>) -> Json<i32> {
+    Json(items::update(&conn, id, item.into_inner()))
+}
+
+#[get("/employees", format = "application/json")]
+fn get_employees(conn: db::DbConn) -> Json<Vec<String>> {
+    Json(employees::get(&conn))
+}
+
+#[get("/holidays", format = "application/json")]
+fn get_holidays(conn: db::DbConn) -> Json<HashMap<String, String>> {
+    Json(holidays::get(&conn))
+}
+
+#[get("/reports", format = "application/json")]
+fn get_reports(conn: db::DbConn) -> Json<Vec<Report>> {
+    Json(reports::get_all(&conn))
+}
+
+#[get("/reports/<id>", format = "application/json")]
+fn get_report(conn: db::DbConn, id: i32) -> Option<Json<Report>> {
+    reports::get(&conn, id).map(|x| Json(x))
+}
+
+#[post("/reports", format = "application/json", data = "<report>")]
+fn add_report(conn: db::DbConn, report: Json<Report>) {
+    reports::add(&conn, report.into_inner());
+}
+
+pub fn routes() -> Vec<::rocket::Route> {
+    routes![
+        item_template,
+        get_reports,
+        get_report,
+        get_globals,
+        get_employees,
+        get_items,
+        get_holidays,
+        add_report,
+        set_item,
+    ]
+}
--- src/main.rs
+++ src/main.rs
@@ -36,11 +36,8 @@ extern crate serde_json;
 use std::path::{Path, PathBuf};
 
 use rocket::response::NamedFile;
-use rocket_contrib::Json;
 
-use items::{InvoiceItem, NewRow};
-use reports::Report;
-
+mod api;
 pub mod db;
 pub mod employees;
 pub mod holidays;
@@ -51,21 +48,6 @@ pub mod weeks;
 
 pub const DATE_FORMAT: &str = "%Y-%m-%d";
 pub const TIME_FORMAT: &str = "%H:%M";
-
-#[derive(Serialize)]
-pub struct Globals {
-    pub mintime: &'static str,
-    pub maxtime: &'static str,
-}
-
-impl Globals {
-    pub fn new() -> Globals {
-        Globals {
-            mintime: "12:30",
-            maxtime: "16:00",
-        }
-    }
-}
 
 #[get("/")]
 fn index() -> Option<NamedFile> {
@@ -80,51 +62,6 @@ fn index() -> Option<NamedFile> {
 fn files(file: PathBuf) -> Option<NamedFile> {
     NamedFile::open(Path::new(concat!(env!("CARGO_MANIFEST_DIR"), "/frontend/dist/")).join(file))
         .ok()
-}
-
-#[get("/globals", format = "application/json")]
-fn get_globals() -> Json<Globals> {
-    Json(Globals::new())
-}
-
-#[get("/items", format = "application/json")]
-fn get_items(conn: db::DbConn) -> Json<Vec<InvoiceItem>> {
-    Json(items::get(&conn))
-}
-
-#[get("/items/template", format = "application/json")]
-fn item_template(conn: db::DbConn) -> Json<InvoiceItem> {
-    Json(items::template(&conn))
-}
-
-#[put("/items/<id>", format = "application/json", data = "<item>")]
-fn set_item(conn: db::DbConn, id: i32, item: Json<NewRow>) -> Json<i32> {
-    Json(items::update(&conn, id, item.into_inner()))
-}
-
-#[get("/employees", format = "application/json")]
-fn get_employees(conn: db::DbConn) -> Json<Vec<String>> {
-    Json(employees::get(&conn))
-}
-
-#[get("/holidays", format = "application/json")]
-fn get_holidays(conn: db::DbConn) -> Json<std::collections::HashMap<String, String>> {
-    Json(holidays::get(&conn))
-}
-
-#[get("/reports", format = "application/json")]
-fn get_reports(conn: db::DbConn) -> Json<Vec<Report>> {
-    Json(reports::get_all(&conn))
-}
-
-#[get("/reports/<id>", format = "application/json")]
-fn get_report(conn: db::DbConn, id: i32) -> Option<Json<Report>> {
-    reports::get(&conn, id).map(|x| Json(x))
-}
-
-#[post("/reports", format = "application/json", data = "<report>")]
-fn add_report(conn: db::DbConn, report: Json<Report>) {
-    reports::add(&conn, report.into_inner());
 }
 
 fn main() {
@@ -143,19 +80,7 @@ fn main() {
     rocket::ignite()
         .manage(db::init_pool())
         .mount("/", routes![index, files])
-        .mount(
-            "/api/",
-            routes![
-                item_template,
-                get_reports,
-                get_report,
-                get_globals,
-                get_employees,
-                get_items,
-                get_holidays,
-                set_item,
-            ],
-        )
+        .mount("/api/", api::routes())
         .attach(options)
         .launch();
 }
