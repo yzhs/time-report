7c07076c534cf7a0457ff07f59b80e87e74785dc
Colin Benner <colin@yzhs.de>
2018-03-17 09:48:33 +0100

    backend: Remove old LaTeX generating code

--- src/generate_pdf.rs
+++ src/generate_pdf.rs
@@ -5,17 +5,12 @@ use std::process;
 
 use csv;
 use diesel::SqliteConnection;
-use time;
 use tempdir::TempDir;
 
 use items::InvoiceItem;
 use reports::{self, Report};
 use DATE_FORMAT;
 use TIME_FORMAT;
-
-const NUM_WORKERS: usize = 100;
-
-const TABLE_HEADER: [&str; 5] = ["Name", "Datum", "von", "bis", "Woche/Bemerkung"];
 
 /// Represents a row in the table containing the date a particular person worked on, how many
 /// hours and minutes they worked, and a free-text comment filed.
@@ -48,27 +43,6 @@ impl Worker {
             minutes: 0,
             lines: vec![],
         }
-    }
-
-    pub fn to_latex(&self) -> String {
-        let mut result = String::with_capacity(500);
-
-        result.push_str("  \\begin{person}{");
-        result.push_str(&self.name);
-        result.push_str("}\n");
-
-        for line in &self.lines {
-            result.push_str(&line.to_latex());
-        }
-
-        result.push_str("    \\midrule\n    \\bfseries{{Summe}} && ");
-        result.push_str(&format!(
-            "\\bfseries{{{}}} & \\bfseries{{{}}}\\\\\n",
-            self.hours, self.minutes
-        ));
-        result.push_str("  \\end{person}\n\n");
-
-        result
     }
 
     pub fn last_name(&self) -> String {
@@ -109,194 +83,7 @@ impl Line {
                 self
             );
         }
-
-        // TODO check date
-        //if self.date > now {
-        //    error!("Date is in the future: {}", self.date);
-        //} else if now - self.date > Duration::years(1) {
-        //    error!("Date is more than one year in the past: {}", self.date);
-        //} else if self.date.tm_wday == 0 || self.date.tm_wday == 6 {
-        //    error!("Date is during a weekend: {}", self.date);
-        //} else if self.date.is_known_holiday() {
-        //    error!("Date is on a holiday: {}", self.date);
-        //}
-
-        // TODO check whether dates increase monotonically
     }
-}
-
-fn check_start_and_end(start: &time::Tm, end: &time::Tm) {
-    // TODO figure out how to report errors with better context
-    if start.tm_hour < 12 {
-        warn!("Start time before noon: {:?}", start);
-    }
-    if end.tm_hour > 16 {
-        warn!("End time after 4 o'clock: {:?}", end);
-    }
-}
-
-/// Read a given CSV file a list of `Line`s.
-fn read_csv_file<P: AsRef<Path>>(path: P) -> csv::Result<Vec<Worker>> {
-    let mut reader = csv::Reader::from_file(path)?
-        .has_headers(true)
-        .flexible(true);
-
-    let headers = try!(reader.headers());
-    let rows: Vec<(String, Line)> = if headers == TABLE_HEADER {
-        info!("Using the automatic duration column set in default order");
-
-        // name, date, start, end, remark
-        type Row = (String, String, String, String, Vec<String>);
-        let row_to_line = |row: Row| {
-            let start = time::strptime(&row.2, "%H:%M").expect("Invalid start time");
-            let end = time::strptime(&row.3, "%H:%M").expect("Invalid end time");
-            check_start_and_end(&start, &end);
-
-            let duration = end - start;
-            let hours = duration.num_hours();
-            let minutes = duration.num_minutes() % 60;
-
-            let line = Line {
-                date: row.1,
-                hours: hours as u16,
-                minutes: minutes as u16,
-                remark: row.4.join(" "),
-            };
-            line.check_data();
-            (row.0, line)
-        };
-
-        let rows: Vec<Row> = try!(reader.decode().collect());
-        rows.into_iter().map(row_to_line).collect()
-    } else if headers == ["Name", "Datum", "Stunden", "Minuten", "Woche/Bemerkung"] {
-        info!("Using the manual duration column set in default order");
-
-        // name, date, hours, minutes, remark
-        type RowManual = (String, String, u16, u16, Vec<String>);
-        let row_to_line = |row: RowManual| {
-            let line = Line {
-                date: row.1,
-                hours: row.2 as u16,
-                minutes: row.3 as u16,
-                remark: row.4.join(" "),
-            };
-            line.check_data();
-            (row.0, line)
-        };
-
-        let rows: Vec<RowManual> = try!(reader.decode().collect());
-        rows.into_iter().map(row_to_line).collect()
-    } else {
-        panic!("Invalid headers: {:?}", headers);
-    };
-
-    // Collect the data for each of the workers
-    use std::collections::HashMap;
-    let mut workers = HashMap::with_capacity(NUM_WORKERS);
-    for (name, line) in rows {
-        let mut person = workers
-            .entry(name.clone())
-            .or_insert_with(|| Worker::new(name));
-        person.hours += line.hours as u32;
-        person.minutes += line.minutes as u32;
-        person.lines.push(line);
-    }
-
-    // Produce a list of workers and handle carry
-    let mut result: Vec<_> = workers
-        .into_iter()
-        .map(|(_, mut person)| {
-            person.hours += person.minutes / 60;
-            person.minutes %= 60;
-            person
-        })
-        .collect();
-
-    // Sort the list by name
-    result.sort_by_key(|k| k.last_name());
-
-    Ok(result)
-}
-
-const LATEX_HEADER_1: &[u8] = b"\\documentclass[a4paper]{article}
-\\usepackage[ngerman]{babel}
-\\usepackage{booktabs} % Nicer vertical lines
-\\usepackage{tabularx} % paragraphs in tables
-\\usepackage{fullpage} % Use the entire page
-\\usepackage{fontspec} % Allow the use of unicode
-\\usepackage{array} % Provide b column type for bottom alignment
-
-%\\renewcommand{\\arraystretch}{1.2}
-
-\\newcommand*{\\headerfor}[1]{%
-  \\bfseries{#1} & \\textsc{Datum} & \\textsc{Stunden} & \\textsc{Minuten} &
-  \\textsc{Woche/Bemerkung}\\\\
-  \\midrule
-}
-\\newenvironment{person}[1]{%
-  \\begin{tabular*}{0.96\\linewidth}{b{0.3\\textwidth}rrrp{0.28\\textwidth}}
-    \\headerfor{#1}
-}{%
-  \\end{tabular*}\\vspace{1cm}
-}
-
-\\title{Abrechnung BetreuerInnen ";
-
-const LATEX_HEADER_2: &[u8] = b"\\vspace{-1cm}}
-\\author{}
-\\date{\\today}
-
-\\begin{document}
-\\maketitle
-";
-
-const LATEX_FOOTER: &[u8] = b"\\end{document}\n";
-
-/// Create a PDF file from the given workers' data.
-pub fn generate_pdf<P: AsRef<Path>>(input: P, workers: &[Worker]) -> Result<(), io::Error> {
-    let input = input.as_ref();
-    let title = input.file_stem().unwrap().to_str().unwrap();
-
-    let dir = TempDir::new("generate-pdf")?;
-
-    // Generate LaTeX file
-    let file_path = dir.path().join("abrechnung.tex");
-    let mut f = File::create(&file_path)?;
-
-    // Write LaTeX code
-    f.write_all(LATEX_HEADER_1)?;
-    write!(f, "{}", title)?;
-    f.write_all(LATEX_HEADER_2)?;
-
-    for worker in workers {
-        write!(f, "{}", worker.to_latex())?;
-    }
-
-    f.write_all(LATEX_FOOTER)?;
-
-    // and sync to disc
-    f.sync_all()?;
-    drop(f);
-
-    // Run XeLaTeX
-    {
-        let tempdir_path_string = dir.path().to_str().unwrap();
-        let file_path_string = file_path.to_str().unwrap();
-        process::Command::new("xelatex")
-            .arg("-output-directory")
-            .arg(tempdir_path_string)
-            .arg(file_path_string)
-            .output()
-            .unwrap();
-        // TODO handle LaTeX errors
-    }
-
-    let pdf = file_path.with_extension("pdf");
-    fs::copy(pdf, input.with_extension("pdf"))?;
-
-    dir.close()?;
-
-    Ok(())
 }
 
 pub struct FullReport {
@@ -349,7 +136,5 @@ pub fn generate(conn: &SqliteConnection, id: i32) -> Option<PathBuf> {
     None
 }
 
-pub fn process_csv_file<P: AsRef<Path>>(csv_file: P) {
-    let data = read_csv_file(csv_file.as_ref()).unwrap();
-    generate_pdf(csv_file, &data).unwrap();
+    render_latex(temp_dir, tex_path)
 }
