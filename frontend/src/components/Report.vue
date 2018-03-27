c8134c38fb72c805baa7cb1110f9047d88f14441
Colin Benner <colin@yzhs.de>
2018-03-27 14:48:46 +0200

    frontend: Remove unmodified marks when clicking on new item button

--- frontend/src/components/Report.vue
+++ frontend/src/components/Report.vue
@@ -126,6 +126,9 @@ export default Vue.extend({
     },
 
     newItem () {
+      // The previous record seems to have been finished, so we can mark it as done.
+      this.items[this.items.length - 1].allModified()
+
       axios.get('reports/' + this.report.id + '/items/template')
           .then((response: any) => {
         let item = newItem(response.data)
